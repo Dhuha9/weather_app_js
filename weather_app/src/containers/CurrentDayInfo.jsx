72f073f43103666be022abeee3cfe84aa08383ff
Dhuha9 <dhuha9hasan@gmail.com>
2021-02-13 09:31:05 +0300

    Refactor: Initializing the AllWeatherData Container
    Adding the initial state and component the is needed
    
    Closes #20

--- weather_app/src/containers/CurrentDayInfo.jsx
+++ weather_app/src/containers/CurrentDayInfo.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import WeatherChart from '../components/WeatherChart/WeatherChart';
+import TableWeather from '../components/TableWeather/TableWeather';
 import OneCurrectInfo from '../components/OneCurrectInfo/OneCurrectInfo';
 
-export default function CurrentDayInfo() {
+export default function AllWeatherData() {
+  const [weatherData, setweatherData] = useState({
+    weather: [],
+    isLoaded: false,
+    currentHour: [],
+    next8days: [],
+    next24Hours: [],
+  });
   return (
     <div>
-      <OneCurrectInfo />
+      {weatherData.isLoaded && (
+        <>
+          <WeatherChart />
+          <br />
+          <TableWeather />
+          <OneCurrectInfo />
+        </>
+      )}
     </div>
   );
 }
